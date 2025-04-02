5ef14ce2372ad608c52f451d9f632f067c8c59e3
RhysWhy <31206745+RhysWhy@users.noreply.github.com>
2025-04-02 15:30:24 +0100

    Add 200 ok response

--- src/Server.cs
+++ src/Server.cs
@@ -7,4 +7,5 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+var socket = server.AcceptSocket(); // wait for client
+await socket.SendAsync(System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
