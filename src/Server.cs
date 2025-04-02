e3a97421c17637e56b0ea45e3707addd6f3fac8d
RhysWhy <31206745+RhysWhy@users.noreply.github.com>
2025-04-02 16:53:31 +0100

    Handle receiving data and determining 200 vs 404 on the path

--- src/Server.cs
+++ src/Server.cs
@@ -8,4 +8,15 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 var socket = server.AcceptSocket(); // wait for client
-await socket.SendAsync(System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+
+var requestBuffer = new byte[1024];
+int receivedBytes = await socket.ReceiveAsync(requestBuffer);
+
+var lines = System.Text.Encoding.UTF8.GetString(requestBuffer).Split("\r\n");
+
+var line0Parts = lines[0].Split(" ");
+var (method, path, httpVerb) = (line0Parts[0], line0Parts[1], line0Parts[2]);
+
+var response = path == "/" ?  $"{httpVerb} 200 OK\r\n\r\n" : $"{httpVerb} 404 Not Found\r\n\r\n";
+
+await socket.SendAsync(System.Text.Encoding.UTF8.GetBytes(response));
