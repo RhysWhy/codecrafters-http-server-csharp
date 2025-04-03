02c3a7e557e039f2a8b0bd9d67e0a0b8b5782100
RhysWhy <31206745+RhysWhy@users.noreply.github.com>
2025-04-03 11:15:35 +0100

    Initial commit for concurrent connections

--- src/Server.cs
+++ src/Server.cs
@@ -3,34 +3,42 @@ using System.Net.Sockets;
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-var socket = server.AcceptSocket(); // wait for client
 
-var requestBuffer = new byte[1024];
-int receivedBytes = await socket.ReceiveAsync(requestBuffer);
-
-var lines = System.Text.Encoding.UTF8.GetString(requestBuffer).Split("\r\n");
-
-var line0Parts = lines[0].Split(" ");
-var (method, pathParts, httpVerb) = (line0Parts[0], line0Parts[1].Split("/", StringSplitOptions.RemoveEmptyEntries), line0Parts[2]);
-
-var response = $"{httpVerb} 404 Not Found\r\n\r\n";
-
-if (pathParts == null || pathParts.Length == 0) { response = $"{httpVerb} 200 OK\r\n\r\n"; }
-else if (pathParts[0] == "echo")
+while (true)
 {
-    var content = pathParts[1];
-    response = $"{httpVerb} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n{content}";
+    var socket = server.AcceptSocket(); // wait for client
+    await Task.Run(() => HandleSocket(socket));
 }
-else if (pathParts[0] == "user-agent")
+
+static async Task HandleSocket(Socket socket)
 {
-    var content = string.Empty;
-    if(lines.Any(h => h.StartsWith("User-Agent: ", StringComparison.OrdinalIgnoreCase)))
+    var requestBuffer = new byte[1024];
+    int receivedBytes = await socket.ReceiveAsync(requestBuffer);
+
+    var lines = System.Text.Encoding.UTF8.GetString(requestBuffer).Split("\r\n");
+
+    var line0Parts = lines[0].Split(" ");
+    var (method, pathParts, httpVerb) = (line0Parts[0], line0Parts[1].Split("/", StringSplitOptions.RemoveEmptyEntries), line0Parts[2]);
+
+    var response = $"{httpVerb} 404 Not Found\r\n\r\n";
+
+    if (pathParts == null || pathParts.Length == 0) { response = $"{httpVerb} 200 OK\r\n\r\n"; }
+    else if (pathParts[0] == "echo")
     {
-        var userAgentHeader = lines.First(h => h.StartsWith("User-Agent: ", StringComparison.OrdinalIgnoreCase));
-        content = userAgentHeader.Replace("User-Agent: ", string.Empty, StringComparison.OrdinalIgnoreCase)
-                                 .Replace("\r\n", string.Empty);
+        var content = pathParts[1];
+        response = $"{httpVerb} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n{content}";
     }
-    response = $"{httpVerb} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n{content}";
-}
+    else if (pathParts[0] == "user-agent")
+    {
+        var content = string.Empty;
+        if (lines.Any(h => h.StartsWith("User-Agent: ", StringComparison.OrdinalIgnoreCase)))
+        {
+            var userAgentHeader = lines.First(h => h.StartsWith("User-Agent: ", StringComparison.OrdinalIgnoreCase));
+            content = userAgentHeader.Replace("User-Agent: ", string.Empty, StringComparison.OrdinalIgnoreCase)
+                                     .Replace("\r\n", string.Empty);
+        }
+        response = $"{httpVerb} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n{content}";
+    }
 
-await socket.SendAsync(System.Text.Encoding.UTF8.GetBytes(response));
+    await socket.SendAsync(System.Text.Encoding.UTF8.GetBytes(response));
+}
